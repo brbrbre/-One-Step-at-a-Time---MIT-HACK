ad95b3575ad0de4926ca10720dc37ed801a9f0cd
lamesss <lamees.tliba@gmail.com>
2025-09-14 11:28:26 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -5,13 +5,22 @@ import asyncio
 import httpx
 from dotenv import load_dotenv
 import requests
+import time
 
 load_dotenv()
 
 app = FastAPI()
+print("\n\n\n\n\n")
 
 SUNO_API_TOKEN = os.getenv("SUNO_API_TOKEN")
-current_prompt_bpm = 90
+current_prompt_bpm = 70
+### given vars from kj code
+givenSongBPM = 70
+playTime =30 #seconds
+
+
+
+
 
 last_clip_id = None
 
@@ -124,3 +133,87 @@ async def generate_music():
 
         # Timeout
         return {"error": "Timed out waiting for clip"}, 504
+
+### end of first song
+
+### start of song extension
+
+
+#following or set mode
+yn = "abcd"
+while (yn != "y" and yn != "n"):
+    print ("Is following mode on? (y/n)")
+    yn = input()
+    if (yn != "y" and yn != "n"):
+        print("Sorry please answer 'y' or 'n'\n\n")
+    if (yn == "y") :
+        followingMode = True
+    if (yn == "n"):
+        followingMode = False
+
+
+#psitive reinforcement for this cycle
+yn = "abcd"
+while (yn != "y" and yn != "n"):
+    print ("Has the patient met a goal in this period? (y/n)")
+    yn = input()
+    if (yn != "y" and yn != "n"):
+        print("Sorry please answer 'y' or 'n'\n\n")
+    if (yn == "y") :
+        EventFlagged = True
+    if (yn == "n"):
+        EventFlagged = False
+
+
+#request bpm percent change
+if (not(followingMode)) :
+    print ("What percent would you like to change BPM by:")
+    PTIdeal = input()
+    PTIdeal = int(PTIdeal)
+    PTIdeal = givenSongBPM*(1+PTIdeal/100)
+
+
+# recording steps and time elapsed
+if (true):
+    print ("recording")
+    input ()
+    tStart = time.time()
+    steps = 1
+    while (steps<=4):
+        input ()
+        steps+=1
+
+    tFin = time.time()
+
+
+#33% time increase reduction
+timeCalibrate = 2/3
+
+#finding average speed in bpm
+averageTime = 60*(steps+1)/((tFin-tStart)/timeCalibrate)
+averageTime = round(averageTime)
+
+print ("recording stopped")
+
+
+
+
+# creates prompt for music extension
+if (true):
+    prompt1 = "Extend song by "+str(playTime)+" seconds"
+    promptBPMFol = ", gradually change the BPM to "+str(averageTime)+" over a period of "+str(playTime)+" seconds"
+    promptEvent =", and add an upbeat flourish near the start of this section"
+    if (not(followingMode)) :
+        promptBPMPT = ", gradually change the BPM to "+str(PTIdeal)+" over a period of "+str(playTime)+" seconds"
+
+    currentPromt = prompt1
+    if (followingMode) :
+        currentPromt = currentPromt+promptBPMFol
+    else :
+        currentPromt = currentPromt+promptBPMPT
+    if (EventFlagged) :
+        currentPromt = currentPromt+promptEvent
+
+print (currentPromt)
+
+
